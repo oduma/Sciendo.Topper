783f8839059c9c46f9e4cb3bb92ca760026ea245
oduma <tavi_duma2002@yahoo.co.uk>
2019-11-26 20:13:06 +0000

    Updated the packages and adminservice is working

---
+++ Topper/Sciendo.Topper.Api/Extensions/ServicesExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Sciendo.Topper.Contracts;
+using Sciendo.Topper.Service;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sciendo.Topper.Api.Extensions
+{
+    public static class ServicesExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+        }
+
+        public static void ConfigureLogger(this IServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddSerilog(new LoggerConfiguration()
+                .WriteTo.File("log-.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger()));
+
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+               
+            });
+        }
+
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAdminService, AdminService>();
+        }
+    }
+}
