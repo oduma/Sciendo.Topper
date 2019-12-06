d2f614c9d6e7833781c5904e601d797d2d69a810
oduma <tavi_duma2002@yahoo.co.uk>
2019-12-06 19:08:25 +0000

    Added swagger for the API

--- Topper/Sciendo.Topper.Api/Startup.cs
+++ Topper/Sciendo.Topper.Api/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Sciendo.Topper.Api.Extensions;
+using Microsoft.OpenApi.Models;
 
 namespace Sciendo.Topper.Api
 {
@@ -32,6 +33,12 @@ namespace Sciendo.Topper.Api
             services.ConfigureIISIntegration();
             services.ConfigureServices(Configuration);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Topper.Web.API", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +53,16 @@ namespace Sciendo.Topper.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Topper.Web.API V1");
+            });
 
             app.UseCors("CorsPolicy");
 
